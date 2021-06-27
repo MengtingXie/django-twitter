2296ada66c63c522f7cad680443c6264191313cc
MengtingXie <info@penosaur.ca>
2021-06-26 20:15:41 -0400

    Add UserProfile Model and Admin

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -66,4 +66,8 @@ class SignupSerializer(serializers.ModelSerializer):
             email=email,
             password=password,
         )
+
+        # Create UserProfile object
+        user.profile
+
         return user
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -86,6 +86,7 @@ class AccountViewSet(viewsets.ViewSet):
             }, status=400)
 
         user = serializer.save()
+
         django_login(request, user)
         return Response({
             'success': True,
